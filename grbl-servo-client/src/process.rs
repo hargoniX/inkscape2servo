c02819d98dafdf74f40e467369a6b7e12140aead
hargoniX <hargonix@gmail.com>
2019-01-12 15:11:40 +0100

    document the code and use a bit better variable names at some places

--- grbl-servo-client/src/process.rs
+++ grbl-servo-client/src/process.rs
@@ -7,10 +7,13 @@ fn process_line(line: &str, state: u8, outfile: Option<&mut File>) -> u8 {
     let mut new_state = state;
     let mut new_line = line;
 
+    // If the line already contains a M3 or M5 gcode ignore it and also exclude it from the output file
     if line.contains("M3") || line.contains("M5") {
         return state;
     }
 
+    // If the current line contains a Z gcode compare the new height it sets to our current height
+    // and write a raise or lower the pen gcode to the file accordingly
     if line.contains('Z') {
         let z_value_regex = Regex::new(r".*Z(?P<z_value>\-?\d+\.\d+).*")
             .expect("For some reason a perfectly valid regex did not work.");
@@ -28,6 +31,7 @@ fn process_line(line: &str, state: u8, outfile: Option<&mut File>) -> u8 {
         }
     }
 
+    // If there is an outfile write to it otherwise print to stdout
     match outfile {
         None => println!("{}", new_line),
         Some(x) => writeln!(x, "{}", new_line).expect("Couldnt write to outfile"),
@@ -35,23 +39,29 @@ fn process_line(line: &str, state: u8, outfile: Option<&mut File>) -> u8 {
     new_state
 }
 
+// Manipulates a gcode file so it gets compatible with the servo approach from the grbl fork
 pub fn process(infile: String, outfile: Option<String>) {
     let content = read_file(infile).expect("Input file not found");
     let content_lines = content.lines();
     let mut state = 0;
+
     match outfile {
+        // If there is no output file given write the results to stdout
         None => {
+            // We want the pen to be initially lifted so this is always the first line
             println!("M5 (pen up)\nG4 P1 (wait a ms so the PWM changes can take action)");
+            // Processes every input line and write the result to stdout
             for line in content_lines {
                 state = process_line(line, state, None);
             }
         }
-        Some(x) => {
+        Some(file_name) => {
+            // Open the outfile and write the initial pen up line to it
             let mut outfile = OpenOptions::new()
                 .write(true)
                 .append(false)
                 .create(true)
-                .open(x)
+                .open(file_name)
                 .expect("Couldnt open outfile");
             writeln!(
                 &mut outfile,
@@ -59,6 +69,7 @@ pub fn process(infile: String, outfile: Option<String>) {
             )
             .expect("Couldnt write to outfile");
 
+            // Process the input and write the results to the outfile
             for line in content_lines {
                 state = process_line(line, state, Some(&mut outfile));
             }
--- grbl-servo-client/src/upload.rs
+++ grbl-servo-client/src/upload.rs
@@ -3,12 +3,14 @@ use reqwest;
 use std::collections::HashMap;
 
 pub fn upload(file: String, ip: String, port: Option<String>) {
+    // Reads the input file and puts it into a JSON dictionary
     let mut json_post = HashMap::new();
     json_post.insert("gcode", read_file(file).expect("Couldnt read input file"));
 
+    // Opens a sessions to the given http server and send the JSON dictionary to that server
     let client = reqwest::Client::new();
     let server_url = match port {
-        Some(x) => format!("http://{}:{}/print", ip, x),
+        Some(port) => format!("http://{}:{}/print", ip, port),
         None => format!("http://{}:{}/print", ip, "8080"),
     };
     println!("Uploading...");
@@ -16,6 +18,6 @@ pub fn upload(file: String, ip: String, port: Option<String>) {
         .post(&server_url)
         .json(&json_post)
         .send()
-        .expect("There was an error during sending the request to the server");
+        .expect("There was an error while sending the request to the server");
     println!("Done");
 }
